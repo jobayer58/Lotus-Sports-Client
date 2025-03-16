867b4bd9d94261af3f32f08446823ef4ccc5404f
jobayer58 <jobayerafnan770@gmail.com>
2025-03-17 03:23:29 +0600

    added tooltrip react animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import logo from '../assets/Lotus_Esports_29logo_square.webp'
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
+import { Tooltip } from 'react-tooltip';
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext)
@@ -52,13 +53,19 @@ const Navbar = () => {
                     <div className="">
                         <div className="flex justify-center items-center md:gap-4 gap-1">
                             {
-                                 user?.email && <div><img className="md:w-14 md:h-14 w-10 h-10 rounded-full object-cover" src={user?.photoURL} alt="" /></div>
-                                    
+                                user?.email && <div><img data-tooltip-id="logo-img-tooltip" className="md:w-14 md:h-14 w-10 h-10 rounded-full object-cover" src={user?.photoURL} alt="" />
+                                    <Tooltip id="logo-img-tooltip"  place="left" effect="solid">
+                                        {
+                                            user?.displayName 
+                                        }
+                                    </Tooltip>
+                                </div>
+
                             }
                             {
                                 user && user?.email ? <button onClick={logOut} className='bg-gradient-to-r from-[#f6ea6b] to-[#eef4ad] btn md:px-8 md:py-6 md:text-[20px]'>LOGOUT</button> : <NavLink to='login' className="bg-gradient-to-r from-[#f6ea6b] to-[#eef4ad] btn md:px-8 md:py-6 md:text-[20px]">LOGIN</NavLink>
                             }
-                            
+
                         </div>
                     </div>
                 </div>
